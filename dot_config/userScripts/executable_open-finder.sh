fd1fecafd0958374f3eff5d0a87848a2231d8698
q <phamousq@gmail.com>
2024-11-25 14:03:27 -0600

    symlinking for windsurf

---
+++ dot_config/userScripts/executable_open-finder.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title open finder
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author quinton_pham
+# @raycast.authorURL https://raycast.com/quinton_pham
+
+open .
+
